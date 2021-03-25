6213efc8dcd402ef008baa2585aada58efdddcac
alain arja <79808845+alainarja@users.noreply.github.com>
2021-03-26 00:21:21 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ const withDB = async (operations, res) => {
 app.get('/api/articles/:name', async (req, res) => {
     withDB(async (db) => {
       
-        const articleName = req.params.name;
+    const articleName = req.wadadparams.name;
 
         const articleInfo = await db.collection('articles').findOne({ name: articleName })
         res.status(200).json(articleInfo);
